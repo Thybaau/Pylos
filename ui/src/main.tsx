695c589fca7fcc2d9e2d1f2e006eb5aefb7ed6fa
JZacharie <j.zacharie@gmail.com>
2026-05-30 14:10:00 +0200

    feat: integrate OpenObserve RUM and browser logs for observability and session replay

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -3,6 +3,59 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { openobserveRum } from '@openobserve/browser-rum';
+import { openobserveLogs } from '@openobserve/browser-logs';
+
+const options = {
+  clientToken: 'rumJHwgt82ZCKIFNQk3',
+  applicationId: 'web-application-id',
+  site: 'o2-openobserve.p.zacharie.org',
+  service: 'my-web-application',
+  env: 'production',
+  version: '0.0.1',
+  organizationIdentifier: 'default',
+  insecureHTTP: false,
+  apiVersion: 'v1',
+};
+
+openobserveRum.init({
+  applicationId: options.applicationId,
+  clientToken: options.clientToken,
+  site: options.site,
+  organizationIdentifier: options.organizationIdentifier,
+  service: options.service,
+  env: options.env,
+  version: options.version,
+  trackResources: true,
+  trackLongTasks: true,
+  trackUserInteractions: true,
+  apiVersion: options.apiVersion,
+  insecureHTTP: options.insecureHTTP,
+  defaultPrivacyLevel: 'allow',
+  sessionSampleRate: 100,
+  sessionReplaySampleRate: 50,
+});
+
+openobserveLogs.init({
+  clientToken: options.clientToken,
+  site: options.site,
+  organizationIdentifier: options.organizationIdentifier,
+  service: options.service,
+  env: options.env,
+  version: options.version,
+  forwardErrorsToLogs: true,
+  insecureHTTP: options.insecureHTTP,
+  apiVersion: options.apiVersion,
+});
+
+openobserveRum.setUser({
+  id: "1",
+  name: "Captain Hook",
+  email: "captainhook@example.com",
+});
+
+openobserveRum.startSessionReplayRecording();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
